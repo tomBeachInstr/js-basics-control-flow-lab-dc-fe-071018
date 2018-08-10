8d2b0e2f1b9a20fbb780e35a5eba13e77c1d006c
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-10 00:10:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ function scuberGreetingForFeet(distance) {
   if (distance > 2000) {
     return "I will gladly take your thirty bucks.";
   }
+  if (distance > 2500) {
+    return "No can do.";
+  }
 }
 
 scuberGreetingForFeet(199);
